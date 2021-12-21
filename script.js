e0c0407be9e6f92651ccf86d8b2dfa78686b763b
Isha Zaka <90307713+Ishazaka@users.noreply.github.com>
2021-12-21 11:38:20 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 
+
+ shownotes();
+let addbtn = document.getElementById('addbtn');
+
+addbtn.addEventListener('click' , function(e){
+let addtext = document.getElementById('addtext');
+let notes = localStorage.getItem("notes");
+if(notes == null){
+   notesObj = [];
+}else{
+  notesObj = JSON.parse(notes);
+}
+ notesObj.push(addtext.value);
+localStorage.setItem("notes" ,JSON.stringify(notesObj));
+addtext.value = "" ;
+shownotes();
+})
+
